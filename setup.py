7facc03da2a4fc84106021ec90e131a519d6eb46
Pavan Vachhani <vachhanihpavan@gmail.com>
2020-05-09 07:25:10 +0530

    Python trial package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name="mytest",
+	version="0.0.1",
+	description="just testing how to do it",
+	license='MIT',
+	packages=["mytest"],
+	author="victor MT",
+	author_email="victor@moronalgo.com",
+	keywords=["example"],
+	url="https://github.com/untikiti/helloapp")
